ae1c22e48b5765d6b02bb5f499b468eb8811c85a
zxbetter <itdevin@163.com>
2020-10-04 16:56:15 +0800

    feat(Activiti6): 集成web-editor，修复保存报错的bug

--- activiti6/src/main/java/io/github/zxbetter/activiti6/SpringBootExample.java
+++ activiti6/src/main/java/io/github/zxbetter/activiti6/SpringBootExample.java
@@ -4,7 +4,10 @@ import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class, scanBasePackages = {"io.github.zxbetter.activiti6", "org.activiti.rest.editor"})
+@SpringBootApplication(
+        exclude = SecurityAutoConfiguration.class,
+        scanBasePackages = {"io.github.zxbetter.activiti6", "org.activiti.rest.editor"}
+)
 public class SpringBootExample {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootExample.class, args);
--- activiti6/src/main/java/org/activiti/rest/editor/model/ModelSaveRestResource.java
+++ activiti6/src/main/java/org/activiti/rest/editor/model/ModelSaveRestResource.java
@@ -28,12 +28,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -54,7 +49,7 @@ public class ModelSaveRestResource implements ModelDataJsonConstants {
   
   @RequestMapping(value="/model/{modelId}/save", method = RequestMethod.PUT)
   @ResponseStatus(value = HttpStatus.OK)
-  public void saveModel(@PathVariable String modelId, @RequestBody MultiValueMap<String, String> values) {
+  public void saveModel(@PathVariable String modelId, @RequestParam MultiValueMap<String, String> values) {
     try {
       
       Model model = repositoryService.getModel(modelId);
